14423981ab1d2a23ed10fe86fa5ed604dd103862
coltn <mail@coltn.io>
2024-01-29 22:06:44 -0700

    add hyprland submap for nav/num

--- scripts/rustshare-cif.sh
+++ scripts/rustshare-cif.sh
@@ -1,1 +1,1 @@
-sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.smbcredentials //10.0.0.108/rust /mnt/rustshare
+sudo mount -t cifs -o rw,vers=3.0,credentials=/root/.smbcredentials,nounix,iocharset=utf8,file_mode=0777,dir_mode=0777 //10.0.0.108/rust /mnt/rustshare
