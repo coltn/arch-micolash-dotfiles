15f3b7ed350dbdf82061eda540ef27b8a26637f0
coltn <mail@coltn.io>
2024-11-10 15:30:13 -0700

    some changes

--- fish/config.fish
+++ fish/config.fish
@@ -32,6 +32,12 @@ function update
     echo  "lists updated 😄" && ls ~/.config/applist/
 end
 
+
+### fuzzy find and remove package with preview
+function fzf-remove
+    pacman -Qq | fzf --multi --preview 'pacman -Qi {1}' | xargs -ro sudo pacman -Rns
+end
+
 ### Tailscale up
 function tailup
     figlet -f 3d.flf "Connecting to Tailscale"
@@ -46,6 +52,81 @@ function config
     nvim . vim
 end
 
+### fzf functions
+
+#### tmux
+function fs -d "Switch tmux session"
+  tmux list-sessions -F "#{session_name}" | fzf | read -l result; and tmux switch-client -t "$result"
+end
+
+#### ssh
+function fssh -d "Fuzzy-find ssh host via ag and ssh into it"
+  ag --ignore-case '^host [^*]' ~/.ssh/config | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
+end
+
+#### Completion
+function fzf-complete -d 'fzf completion and print selection back to commandline'
+	# As of 2.6, fish's "complete" function does not understand
+	# subcommands. Instead, we use the same hack as __fish_complete_subcommand and
+	# extract the subcommand manually.
+	set -l cmd (commandline -co) (commandline -ct)
+	switch $cmd[1]
+		case env sudo
+			for i in (seq 2 (count $cmd))
+				switch $cmd[$i]
+					case '-*'
+					case '*=*'
+					case '*'
+						set cmd $cmd[$i..-1]
+						break
+				end
+			end
+	end
+	set cmd (string join -- ' ' $cmd)
+
+	set -l complist (complete -C$cmd)
+	set -l result
+	string join -- \n $complist | sort | eval (__fzfcmd) -m --select-1 --exit-0 --header '(commandline)' | cut -f1 | while read -l r; set result $result $r; end
+
+	set prefix (string sub -s 1 -l 1 -- (commandline -t))
+	for i in (seq (count $result))
+		set -l r $result[$i]
+		switch $prefix
+			case "'"
+				commandline -t -- (string escape -- $r)
+			case '"'
+				if string match '*"*' -- $r >/dev/null
+					commandline -t --  (string escape -- $r)
+				else
+					commandline -t -- '"'$r'"'
+				end
+			case '~'
+				commandline -t -- (string sub -s 2 (string escape -n -- $r))
+			case '*'
+				commandline -t -- (string escape -n -- $r)
+		end
+		[ $i -lt (count $result) ]; and commandline -i ' '
+	end
+
+	commandline -f repaint
+end
+
+#### cd history
+function fzf-cdhist-widget -d 'cd to one of the previously visited locations'
+	# Clear non-existent folders from cdhist.
+	set -l buf
+	for i in (seq 1 (count $dirprev))
+		set -l dir $dirprev[$i]
+		if test -d $dir
+			set buf $buf $dir
+		end
+	end
+	set dirprev $buf
+	string join \n $dirprev | tac | sed 1d | eval (__fzfcmd) +m --tiebreak=index --toggle-sort=ctrl-r $FZF_CDHIST_OPTS | read -l result
+	[ "$result" ]; and cd $result
+	commandline -f repaint
+end
+
 
 ## aliases
 alias vim='nvim'
@@ -57,6 +138,8 @@ alias debianvm='sh ~/vm/debian/run.sh'
 alias windowsvm='sh ~/vm/runwindows.sh'
 alias wobreset='sh ~/.config/scripts/wob.sh'
 alias rustcif='sh ~/.config/scripts/rustshare-cif.sh'
-alias webcam-preview='mpv av://v4l2:/dev/video6 --profile=low-latency --untimed'
+alias webcam-preview='mpv av://v4l2:/dev/video0 --profile=low-latency --untimed'
+alias fzf-install="pacman -Slq | fzf --multi --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S"
+alias fzf-vim='nvim $(fzf -m --preview="bat --color=always {}")'
 
 starship init fish | source
